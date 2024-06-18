24bbdfcedfda6245eb3d68062856284f1ea6e268
shaleen69 <143455272+shaleen69@users.noreply.github.com>
2024-06-19 02:50:53 +0530

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,8 @@
+<script>
+    const toggleButton=document.getElementById('nav-toggle');
+    const navLinks=document.getElementById('nav-links');
+
+    toggleButton.addEventListener('click',() => {
+        navLinks.classList.toggle('active');
+    })
+</script>
